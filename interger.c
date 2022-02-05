0a561220b8f7dcc6fa53af38e499e6b7ca0d2fc9
Kao wei <96038610+Friday965@users.noreply.github.com>
2022-02-06 06:04:38 +0800

    兩個整數的合

---
+++ interger.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int integer1;
+    int integer2;
+    int sum;
+    printf("please enter tne first interger:");
+    scanf("%d",&integer1);
+    printf("please enter tne second interger:");
+    scanf("%d",&integer2);
+    sum=integer1+integer2;
+    printf("sum is %d.\n",sum);
+    return 0;
+    
+    
+}
