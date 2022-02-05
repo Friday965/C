fb5d611afc7e21c456c279fc1a633a6c6f578e83
Kao wei <96038610+Friday965@users.noreply.github.com>
2022-02-06 06:27:15 +0800

    三個整數的合

---
+++ sum of three number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int integer1,integer2,integer3,sum;
+    printf("please enter tne first interger:");
+    scanf("%d",&integer1);
+    printf("please enter tne second interger:");
+    scanf("%d",&integer2);
+    printf("please enter tne third interger:");
+    scanf("%d",&integer3);
+    sum=integer1+integer2+integer3;
+    printf("sum is %d.\n",sum);
+    return 0;
+    
+    
+}
